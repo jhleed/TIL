21c17864b3c3e90af1814f3d3e44f4b68dec72a3
james <jhleed@naver.com>
2017-06-14 14:30:09 +0900

    NodeJs : Express : routing을 사용한 simple server : html 파일 렌더링
     - 참고 : https://velopert.com/294

--- nodejs/express/server.js
+++ nodejs/express/server.js
@@ -1,9 +1,12 @@
 let express = require("express");
 let app = express();
+let router = require("./router/main")(app);
+
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+
 app.listen(3000, function () {
     console.log("express server has started");
-});
-
-app.get("/", function (req, res) {
-    res.send("Hello James");
 });
