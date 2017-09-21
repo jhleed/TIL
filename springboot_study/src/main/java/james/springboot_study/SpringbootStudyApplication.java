1f75e659dcc68d127a3576565fdf29fd44ab4b31
JongHo Lee <jhleed@radnet.kr>
2017-09-21 17:57:14 +0900

    Spring Boot : 새로 추가

---
+++ springboot_study/src/main/java/james/springboot_study/SpringbootStudyApplication.java
@@ -0,0 +1,12 @@
+package james.springboot_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudyApplication.class, args);
+	}
+}
