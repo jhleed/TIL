b58e57f3de034f700296dca8dd2adea9340c38e2
JongHo Lee <jhleed@radnet.kr>
2017-09-22 10:32:04 +0900

    Java : Annotation : 주석 추가

--- java/annotation/example/StringInjector.java
+++ java/annotation/example/StringInjector.java
@@ -7,10 +7,9 @@ import java.lang.annotation.Target;
 
 /**
  * String 문자열을 주입하기 위해 선언하는 어노테이션입니다.
- * FIELD에만 선언가능하고 JVM이 어노테이션 정보를 참조합니다.
  */
-@Target(ElementType.FIELD)
-@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD}) //어노테이션 적용 가능한 위치
+@Retention(RetentionPolicy.RUNTIME) //어노테이션이 적용되는 시점 (런타임 (컴파일 이후) 까지 적용 됨)
 public @interface StringInjector {
     String value() default "This is StringInjector.";
 }
